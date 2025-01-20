9637aff89347a5836f44393ab77bf2d23cb96436
Z-araan <m18755372516@163.com>
2025-01-20 17:00:34 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -14,16 +14,3 @@ document.getElementById("toggle-menu").addEventListener("click", function() {
     }
 });
 
-// 页面加载完成后执行
-<button onclick="downloadApp()">下载APP</button>
-
-<script>
-    function downloadApp() {
-        const link = document.createElement("a");
-        link.href = "https://z-araan.github.io/Download/z-araan.apk";
-        link.download = "好东西"; // 设置下载后的文件名
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
-</script>
