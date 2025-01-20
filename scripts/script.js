c2ae3b7373ee6fc8316e82e43528cfb232b8b4cd
Z-araan <m18755372516@163.com>
2025-01-20 22:05:44 +0800

    更新 script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,4 +13,21 @@ document.getElementById("toggle-menu").addEventListener("click", function() {
         menu.style.display = "block";
     }
 });
+document.getElementById("downloadButton").addEventListener("click", function() {
+            // 创建一个虚拟的 <a> 标签
+            const link = document.createElement("a");
+
+            // 设置文件路径
+            link.href = "/Down/z-araan.akp"; // 替换为实际文件路径
+            link.download = "z-araan.akp"; // 设置下载文件的名称
+
+            // 将 <a> 标签添加到页面中
+            document.body.appendChild(link);
+
+            // 模拟点击
+            link.click();
+
+            // 移除 <a> 标签
+            document.body.removeChild(link);
+        });
 
