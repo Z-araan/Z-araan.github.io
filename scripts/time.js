5d6a33bb5fad8713460fe20aa07c2bb89140b102
Z-araan <m18755372516@163.com>
2025-01-23 20:28:27 +0800

    更新 time.js

--- scripts/time.js
+++ scripts/time.js
@@ -12,13 +12,4 @@ function showTime() {
         setInterval(showTime, 1000); // 每隔1秒调用一次showTime函数
         showTime(); // 页面加载时立即显示时间
         
-        document.getElementById('search-input').addEventListener('keypress', function(event) {
-        if (event.keyCode === 13)
-        if (event.key === 'Enter')
-         { // 回车键的键码是13
-        
-            document.querySelector('form').submit(); // 触发表单提交
-        }
-    });
-
         
